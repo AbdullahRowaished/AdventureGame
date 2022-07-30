d9e168e2223ed1483586c3cf5114244fd2cb86d9
Abdullah Rowaished <abdullah_rowaished@outlook.sa>
2022-07-30 20:08:23 +0300

    Initial Commit

---
+++ venv/Scripts/Assets/BlitGraphics.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.locals import *
+from sys import *
+from math import ceil
+
+SCREEN_SIZE = (1280, 720)
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+cursor = pygame.transform.scale(pygame.image.load("Images\Spear of Longinus.png").convert_alpha(), [160,90])
+pygame.mouse.set_visible(False)
+
+def ScreenUpdate():
+    screen.fill((25, 25, 25))
+
+def CursorUpdate(event):
+    ScreenUpdate()
+    screen.blit(cursor, pygame.mouse.get_pos())
+    
+def NewButton(size):
+    width, height = SCREEN_SIZE
+    return pygame.surface.Surface([width * size[0], height * size[1]])
+
+def TitleScreen():
+    PressAnyButton = NewButton([0.3, 0.1])
+    LoadGame = NewButton([0.3, 0.1])
+    NewGame = NewButton([0.3, 0.1])
+    ExitButton = NewButton([0.3, 0.1])
+    
+    START = 0
+    MAIN_MENU = 1
+    LOAD_GAME = 2
+    NEW_GAME = 3
+    EXIT_GAME = 4
+    
+    ScreenUpdate()
+    screen.blit(PressAnyButton, [ceil(SCREEN_SIZE[0]*0.5), ceil(SCREEN_SIZE[1]*0.25)])
+    
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+        elif event.type == MOUSEMOTION:
+            CursorUpdate(event)
+
+    pygame.display.update()
