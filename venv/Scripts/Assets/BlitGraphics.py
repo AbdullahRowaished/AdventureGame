d8b20ba41271a129a8fd2613ef23a30e91a4f1ac
Abdullah Rowaished <abdullah_rowaished@outlook.sa>
2022-08-01 21:38:44 +0300

    feat: centered 'Press Any Button' text on button.

--- venv/Scripts/Assets/BlitGraphics.py
+++ venv/Scripts/Assets/BlitGraphics.py
@@ -20,28 +20,19 @@ def NewButton(size):
     width, height = SCREEN_SIZE
     return pygame.surface.Surface([width * size[0], height * size[1]])
 
-def TitleScreen():
+def TitleScreenUpdate():
     PressAnyButton = NewButton([0.3, 0.1])
-    LoadGame = NewButton([0.3, 0.1])
-    NewGame = NewButton([0.3, 0.1])
-    ExitButton = NewButton([0.3, 0.1])
-    
-    START = 0
-    MAIN_MENU = 1
-    LOAD_GAME = 2
-    NEW_GAME = 3
-    EXIT_GAME = 4
 
     PressAnyButton.fill([255,0,0])
-    pressanytext = pygame.font.SysFont("arial",22).render("Press Any Button", True, [0,0,0], [255,255,255])
+    pressanytext = pygame.font.SysFont("arial",22).render("Press Any Button", True, [255,255,255], PressAnyButton.get_colorkey())
+    PressAnyButton.blit(pressanytext, [(PressAnyButton.get_width()-pressanytext.get_width())/2, (PressAnyButton.get_height()-pressanytext.get_height())/2])
     screen.blit(PressAnyButton, [ceil(SCREEN_SIZE[0]*0.35), ceil(SCREEN_SIZE[1]*0.7)])
-    PressAnyButton.blit(pressanytext, [0,0])
     
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
     ScreenUpdate()
-    TitleScreen()
+    TitleScreenUpdate()
     CursorUpdate(event)
     pygame.display.update()
